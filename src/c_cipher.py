99abc3ca966b05857bfd96e059ad039ef07b1614
Tristan-M-06 <tmm113@sunyorange.edu>
2024-10-01 19:58:16 -0400

    Update c_cipher.py Should be Final One

--- src/c_cipher.py
+++ src/c_cipher.py
@@ -6,22 +6,30 @@
     Changes:
         20240926 - Added framing comment
         20240917 - Added Documentation comments, Formatted comments
+        20241001 - Got rid of literals throughout the code so it would accept any argument.
+                   Added more comments and altered docstrings
 '''
 import logging
 logging.basicConfig(level=logging.INFO, format='%(message)s')
 
 def encrypt(email):
     """
-    TODO: What is the objective? 
+    Objective:
+        The encrypt method: should receive a String argument and shift each element up 3 ASCII
+        if the conditions are met
 
     Args:
-        TODO: what arguments and data types are expected? (i.e., email)
+        String email: text String (should be six characters, 3 letters followed by 3 numbers)
 
     Returns:
-        TODO: what varibale and data types are being returned?   
+        output: input validation message letting the user know the argument was invalid
+        retVal: a String with each element shifted up 3 ASCII from the argument   
     """
     output = "" 
+    
+    #input validation to make sure the email String has a length of 6
     len_flag = len(email) != 6
+    
     # TODO: fix line below and, implement functionality rather than literals
     # keep all updates in the anum_flag (bool) variable
     # i.e., 
@@ -42,9 +50,11 @@ def encrypt(email):
         return output     
         
     # TODO: fix line below, process our string into a list
+    # Turns the email String into a list where each element is a String of each character
     email_lst = [email[0], email[1], email[2], email[3], email[4], email[5]]
         
     # TODO: complete line(s) below, convert EACH new element into a string
+    # Converts each element into a string of ASCII 3 above what the current element is
     new_ascii = ord(email_lst[0]) + 3    # NOTE: here we extract and update element at 0 
     email_lst[0] = chr(new_ascii)        # NOTE: here we convert our ASCII into string
 
@@ -64,6 +74,7 @@ def encrypt(email):
     email_lst[5] = chr(new_ascii)
 
     # TODO: fix line below, convert list into a string
+    #uses the join function to form a String of all the elements in the email_lst list
     email_str = ''.join(email_lst)
 
     # keep all updates in the retVal (str) variablei
@@ -71,18 +82,25 @@ def encrypt(email):
     #    email_str = " some string updates here "
     #    email_1 = email_str.strip()
     #    retVal = email_1
+    
+    #Declares and initializes the retVal variable with the String within the email_str variable
     retVal = email_str
+
+    #returns the String stored within the retVal variable
     return retVal 
 
 def decrypt(email):
     """
-    TODO: What is the objective? 
+    Objective:
+        The decrypt method: Should receive a String argument and shift each element down 3 ASCII
+        if the conditions are met 
 
     Args:
-        TODO: what arguments and data types are expected? (i.e., email)
+        String email: text String (should be six characters, 3 letters followed by 3 numbers)
 
     Returns:
-        TODO: what varibale and data types are being returned?   
+        output: input validation message letting the user know the argument was invalid
+        retVal: a String with each element shifted up 3 ASCII from the argument 
     """
     # input validation
     output = "" 
@@ -91,8 +109,10 @@ def decrypt(email):
     # keep all updates in the anum_flag (bool) variable
     # i.e., 
 
+    # Turns the email String into a list where each element is a String of each character
     email_lst = [email[0], email[1], email[2], email[3], email[4], email[5]]
-    
+
+    # Converts each element into a string of ASCII 3 below what the current element is
     old_ascii = ord(email_lst[0]) - 3
     email_lst[0] = chr(old_ascii)
     
@@ -135,5 +155,9 @@ def decrypt(email):
     #    email_str = email
     #    email_1 = email_str.strip()
     #    retVal = email_1
-    retVal = "aef345"
+
+    #Declares and initializes the retVal variable with the String within the email variable
+    retVal = email
+    
+    #returns the String stored within the retVal variable
     return retVal
